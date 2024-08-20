502e924db174a6018ea4fe5ddca35aadbce631fe
pamelacrist <pamelacrist219@gmail.com>
2024-08-19 23:46:16 -0300

    Iniciando criação da tela Home do site

---
+++ src/componentes/Banner.js/Banner.js
@@ -0,0 +1,11 @@
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header className="banner">
+      <img src="Imagens/banner.png" alt="Banner"></img>
+    </header>
+  );
+}
+
+export default Banner;
